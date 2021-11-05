11df125beea581e6ce1c93e05b669047c0fef897
benja <benja@DESKTOP-CERS98O>
2021-11-05 02:44:17 +0100

    Add project files.

---
+++ Assignment1_/Taskes.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+/*int main()
+{
+	int n;
+	cout << "type a number"<< endl;
+	cin >> n;
+
+	cout << "hello world" << endl;
+	for (int i = 0; i < n; i = i + 1) {
+		cout << "hello world" << endl;
+	}
+	int e = 0;
+	while (e < n) {
+			cout << "hello world" << endl;
+			e++;
+	}
+	int f = 0;
+	do {
+
+		cout << "hello world" << endl;
+		f++;
+
+	} while (f < n);
+
+	return 0;
+
+
+}*/
+/*
+int main()
+{
+	string name;
+	cout << "What is your name" << endl;
+	cin >> name;
+
+	int age;
+	cout << "How old are you?" << endl;
+	cin >> age;
+
+	char gender;
+	cout << "Are you male(m) or female(f)" <<endl;
+	cin >> gender;
+
+	string adress;
+	cout << "What is your adress?" <<endl;
+	cin >> adress;
+
+	bool married;
+	cout << "Are you married, true(1) or false(0)?" <<endl;
+	cin >> married;
+
+	float lenght;
+	cout << "How tall are you?" <<endl;
+	cin >> lenght;
+	}
+	*/
+
+int main()
+{
+	char name;
+	printf_s("input");
+	scanf_s(name);
+}
